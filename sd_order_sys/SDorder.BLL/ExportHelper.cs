bf0c97349987a400d3de313e4bc8f005afe26b18
bonjovirem <slamdunk531@sina.com>
2017-01-08 23:17:02 +0800

    添加

--- sd_order_sys/SDorder.BLL/ExportHelper.cs
+++ sd_order_sys/SDorder.BLL/ExportHelper.cs
@@ -2,16 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace SDorder.BLL
 {
-    public static class ExportHelper //导出工具类   修改！！！！！！by wh
+    public static class ExportHelper //导出工具类  
     {
         /// <summary>
         /// 数据写入模板类
         /// </summary>
         public static void WriteToTemplate()
         {
+        }
+        /// <summary>
+        /// 创建项目目录
+        /// </summary>
+        /// <param name="path">项目路径</param>
+        /// <param name="projectName">项目名称</param>
+        public static void CreateFile(string path, string projectName)
+        {
+            if (!Directory.Exists(path + @"/project/" + projectName))
+            {
+                Directory.CreateDirectory(path + @"/project/" + projectName);
+            }
+            //将模板复制到项目文件夹中
+            List<string> templateFiles = new List<string>();//模板文件集合
+            string templatePath = path + @"/template/";
+            string projectPath = path + @"/project/" + projectName;
+            foreach (string name in templateFiles)
+            {
+                File.Copy(templatePath + name, projectPath + name, true);
+            }
         }
     }
 }
---
+++ sd_order_sys/SDorder.BLL/SqlManage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+
+namespace SDorder.BLL
+{
+    public static class SqlManage
+    {
+        public static DataSet Query(string sql, Dictionary<string, object> sqlparams)
+        {
+            MySqlCommand sqlcom = new MySqlCommand();
+            sqlcom.CommandText = sql;
+            MySqlParameter[] param = new MySqlParameter[sqlparams.Keys.Count];
+            int num = 0;
+            foreach (string key in sqlparams.Keys)
+            {
+                param[num] = new MySqlParameter(key, sqlparams[key]);
+                num++;
+            }
+            return SDorder.DAL.MySqlHelper.GetDataSet(SDorder.DAL.MySqlHelper.connectionStringManager, sql, param);
+        }
+    }
+}
