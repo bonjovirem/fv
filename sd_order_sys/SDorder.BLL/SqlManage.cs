847ebf180fc72770fd1df61f9a0d6be9676937f1
bonjovirem <slamdunk531@sina.com>
2017-01-09 20:41:51 +0800

    增加记录增删改操作方法

--- sd_order_sys/SDorder.BLL/SqlManage.cs
+++ sd_order_sys/SDorder.BLL/SqlManage.cs
@@ -23,5 +23,49 @@ namespace SDorder.BLL
             }
             return SDorder.DAL.MySqlHelper.GetDataSet(SDorder.DAL.MySqlHelper.connectionStringManager, sql, param);
         }
+        /// <summary>
+        /// 记录操作类：增删改
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="sqlparams"></param>
+        /// <returns></returns>
+        public static bool OpRecord(string sql, Dictionary<string, object> sqlparams)
+        {
+            MySqlCommand sqlcom = new MySqlCommand();
+            sqlcom.CommandText = sql;
+            MySqlParameter[] param = new MySqlParameter[sqlparams.Keys.Count];
+            int num = 0;
+            foreach (string key in sqlparams.Keys)
+            {
+                param[num] = new MySqlParameter(key, sqlparams[key]);
+                num++;
+            }
+            int result = SDorder.DAL.MySqlHelper.ExecuteNonQuery(SDorder.DAL.MySqlHelper.connectionStringManager, CommandType.Text,
+                sql, param);
+            if (result > 0)
+                return true;
+            else
+                return false;
+        }
+        /// <summary>
+        /// 查询是否存在记录
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="sqlparams"></param>
+        /// <returns></returns>
+        public static int Exists(string sql, Dictionary<string, object> sqlparams)
+        {
+            MySqlCommand sqlcom = new MySqlCommand();
+            sqlcom.CommandText = sql;
+            MySqlParameter[] param = new MySqlParameter[sqlparams.Keys.Count];
+            int num = 0;
+            foreach (string key in sqlparams.Keys)
+            {
+                param[num] = new MySqlParameter(key, sqlparams[key]);
+                num++;
+            }
+            return (int)SDorder.DAL.MySqlHelper.ExecuteScalar(SDorder.DAL.MySqlHelper.connectionStringManager, CommandType.Text,
+                sql, param);
+        }
     }
 }
